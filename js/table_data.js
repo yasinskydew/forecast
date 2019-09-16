69fcef5da547adc5d2311cee46a460ea1e3a03f4
unknown <yasinsky2015@gmail.com>
2019-09-16 13:42:54 +0300

    add icons

--- js/table_data.js
+++ js/table_data.js
@@ -4,6 +4,16 @@ const createEl = (data, tag = 'td') => {
     return el
 } 
 
+const createIcon = (data) => {
+    const el = document.createElement('td');
+    const name = data.icon;
+    const img = document.createElement('img')
+    img.src = `./icons/${name}.png`
+    img.style.width = "40%"
+    el.append(img)
+    return el
+}
+
 const removeTableEl = () => {
     const table = document.getElementById('table-data')
     while (table.firstChild) {
@@ -12,11 +22,13 @@ const removeTableEl = () => {
 }
 
 const appendTableEl = (dataObject) => {
-    const {valid_date, max_temp, min_temp, temp, wind_spd, wind_cdir_full} = dataObject
+    console.log(dataObject)
+    const {valid_date, max_temp, min_temp, temp, wind_spd, wind_cdir_full, weather} = dataObject
     const table = document.getElementById('table-data')
     const tr = document.createElement('tr')
     table.append(tr)
     tr.append(createEl(valid_date, 'th'))
+    tr.append(createIcon(weather))
     tr.append(createEl(temp))
     tr.append(createEl(max_temp))
     tr.append(createEl(min_temp))
